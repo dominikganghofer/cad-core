3023beccf928b05c8de3690cb523e1b86a14349d
dominikganghofer <gdominik@posteo.de>
2021-01-26 21:38:52 +0100

    Migrate ModelSerialization to submodule

---
+++ Model/ModelSerialization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PCAD.Model
+{
+    [Serializable]
+    public class ModelSerialization
+    {
+        public CoordinateSystem.SerializableCoordinateSystem cs;
+        public List<PointModel.Serialization> points;
+        public List<LineModel.Serialization> lines;
+        public List<RectangleModel.Serialization> rectangles;
+    }
+}
