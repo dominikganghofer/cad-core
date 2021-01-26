d3a872eb24e757a11237a8241b102bc4e66efff3
dominikganghofer <gdominik@posteo.de>
2021-01-26 21:24:28 +0100

    Move logic out of CoordinateSystem model

--- Model/CoordinateLayer/CoordinateSystem.cs
+++ Model/CoordinateLayer/CoordinateSystem.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using PCAD.UserInput;
 
 namespace PCAD.Model
 {
@@ -59,74 +58,7 @@ namespace PCAD.Model
             Anchor = new Anchor(xAnchorCoordinate, yAnchorCoordinate, zAnchorCoordinate);
         }
 
-        /// <summary>
-        /// Get a Vector of Coordinates for a given drawing context
-        /// </summary>
-        /// <param name="position"></param>
-        /// <param name="distancesToAnchor"></param>
-        /// <param name="asPreview"></param>
-        /// <param name="keyboardInput"></param>
-        /// <returns></returns>
-        public Vec<Coordinate> GetParametricPosition(Vec<float> position, Vec<float> distancesToAnchor, bool asPreview,
-            NumpadInput.Model keyboardInput)
-        {
-            SnappedCoordinate = new Vec<Coordinate>();
-            SnappedParameter = new Vec<Parameter>();
-
-            var output = new Vec<Coordinate>();
-            foreach (var a in Vec.XYZ)
-            {
-                // a parameter is referenced in the keyboard input
-                if (keyboardInput?.ParameterReferences[a] != null)
-                {
-                    output[a] = Axes[a].AddNewMueCoordinateWithParameterReference(
-                        keyboardInput.ParameterReferences[a],
-                        keyboardInput.IsDirectionNegative[a],
-                        asPreview
-                    );
-                    continue;
-                }
-
-                // a dimension is set in the keyboard input
-                if (keyboardInput?.DimensionInput[a] != null)
-                {
-                    output[a] = Axes[a].AddNewMueCoordinateWithParameterValue(
-                        keyboardInput.DimensionInput[a].InM,
-                        keyboardInput.IsDirectionNegative[a],
-                        asPreview
-                    );
-                    continue;
-                }
-
-                // try to snap to an existing coordinate
-                var snappedCoordinate = Axes[a].TryToSnapToExistingCoordinate(position[a], asPreview);
-                if (snappedCoordinate != null)
-                {
-                    output[a] = snappedCoordinate;
-                    SnappedCoordinate[a] = snappedCoordinate;
-                    continue;
-                }
-
-                // try to snap to an existing parameter
-                var parameterSnap = Axes[a].TryToSnapToExistingParameter(distancesToAnchor[a], GetAllParameters());
-                if (parameterSnap != null)
-                {
-                    output[a] = Axes[a].AddNewMueCoordinate(
-                        parameterSnap.Value.parameter,
-                        parameterSnap.Value.pointsInNegativeDirection,
-                        asPreview
-                    );
-                    SnappedParameter[a] = parameterSnap.Value.parameter;
-                    continue;
-                }
-
-                // create new coordinate
-                output[a] = Axes[a].AddNewMueCoordinate(position[a], asPreview);
-            }
-
-            return output;
-        }
-
+       
         public Axis AxisThatContainsCoordinate(Coordinate c)
         {
             foreach (Axis a in Axes)
